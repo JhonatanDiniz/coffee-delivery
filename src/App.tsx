a0453f17e183d1da057bc018c9d912a0b2f7ccdd
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2024-05-29 08:51:35 -0300

    Projeto iniciado e configurado estilos globais

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/theme/default"
+
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+      <h1>Olá mundo!</h1>
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${(props) => props.theme['background']};
+    color: ${(props) => props.theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: "Roboto", sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: "Baloo 2", sans-serif;
+  }
+`
+
